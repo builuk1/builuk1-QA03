2890e3f428f659f3f2c6e8af182fb4dc061fe844
Green <name>
2022-01-27 20:18:27 +0200

    Lesson11

--- Lesson11.py
+++ Lesson11.py
@@ -9,6 +9,8 @@ while game:
     player2_choice = ''
     player1_score = 0
     player2_score = 0
+    player1_name = str(input("Enter your name player 1 : "))
+    player2_name = str(input("Enter your name player 2 : "))
     rounds = 3
 
     #Start of game
@@ -17,9 +19,9 @@ while game:
         player2_choice = ''
         #Enter data
         while player1_choice != 'r' and  player1_choice != 'p' and player1_choice != 's':
-            player1_choice = str(input("Enter your choice player 1: [r],[p],[s] : "))
+            player1_choice = str(input(f"Enter your choice {player1_name}: [r],[p],[s] : "))
         while player2_choice != 'r' and player2_choice != 'p' and player2_choice != 's':
-            player2_choice = str(input("Enter your choice player 2: [r],[p],[s] : "))
+            player2_choice = str(input(f"Enter your choice {player2_name}: [r],[p],[s] : "))
         #Compare data
 
         # if player1_choice == 'r':
@@ -35,31 +37,31 @@ while game:
             print('Draw round')
         elif player1_choice == 'r':
             if player2_choice == 's':
-                print('Player 1 win the round!')
+                print(f'{player1_name} win the round!')
                 player1_score = player1_score + 1
             else:
-                print('Player 2 win the round!')
+                print(f'{player2_name} win the round!')
                 player2_score = player2_score + 1
         elif player1_choice == 'p':
             if player2_choice == 'r':
-                print('Player 1 win the round!')
+                print(f'{player1_name} win the round!')
                 player1_score = player1_score + 1
             else:
-                print('Player 2 win the round!')
+                print(f'{player2_name} win the round!')
                 player2_score = player2_score + 1
         elif player1_choice == 's':
             if player2_choice == 'p':
-                print('Player 1 win the round!')
+                print(f'{player1_name} win the round!')
                 player1_score = player1_score + 1
             else:
-                print('Player 2 win the round!')
+                print(f'{player2_name} win the round!')
                 player2_score = player2_score + 1
     #Compare score
     if player1_score > player2_score:
-        print('Player 1 win the game!')
+        print(f'{player1_name} win the game!')
         global_player1_score = global_player1_score + 1
     elif player2_score > player1_score:
-        print('Player 2 win the game!')
+        print(f'{player2_name} win the game!')
         global_player2_score = global_player2_score + 1
     else:
         print('Draw game!')
