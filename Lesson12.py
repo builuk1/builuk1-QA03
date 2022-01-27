228dfb0dcd068cb199d094ea1fef808023d7b2cd
Green <name>
2022-01-27 21:25:32 +0200

    Lesson12 Final

--- Lesson12.py
+++ Lesson12.py
@@ -16,6 +16,7 @@ board_7 = '7'
 board_8 = '8'
 board_9 = '9'
 turn = 'X'
+winner = ''
 #Show board (simple)
 # print(board_1,board_2,board_3)
 # print(board_4,board_5,board_6)
@@ -60,59 +61,59 @@ while count <= 9:
     #Check winner(simple)
     if board_1 == board_2 == board_3:
         if board_1 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_1 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_4 == board_5 == board_6:
         if board_4 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_4 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_7 == board_8 == board_9:
         if board_7 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_7 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_1 == board_4 == board_7:
         if board_1 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_1 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_2 == board_5 == board_8:
         if board_2 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_2 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_3 == board_6 == board_9:
         if board_3 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_3 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_7 == board_5 == board_3:
         if board_7 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_7 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_1 == board_5 == board_9:
         if board_1 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_1 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     #Show board (pro)
     board = f'{board_1}|{board_2}|{board_3}\n-----\n{board_4}|{board_5}|{board_6}\n-----\n{board_7}|{board_8}|{board_9}'
@@ -155,58 +156,64 @@ while count <= 9:
     #Check winner(simple)
     if board_1 == board_2 == board_3:
         if board_1 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_1 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_4 == board_5 == board_6:
         if board_4 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_4 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_7 == board_8 == board_9:
         if board_7 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_7 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_1 == board_4 == board_7:
         if board_1 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_1 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_2 == board_5 == board_8:
         if board_2 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_2 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_3 == board_6 == board_9:
         if board_3 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_3 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_7 == board_5 == board_3:
         if board_7 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_7 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
     if board_1 == board_5 == board_9:
         if board_1 == player1_symbol:
-            print('Player 1 win!')
+            winner = 'X'
             break
         elif board_1 == player2_symbol:
-            print('Player 2 win!')
+            winner = 'O'
             break
 
+if winner == 'X':
+    print('Player 1 win the game!')
+elif winner == 'O':
+    print('Player 2 win the game!')
+elif winner == '':
+    print('Draw')
