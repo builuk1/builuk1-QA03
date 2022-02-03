b5f42b4767630ce3988a2462ad255715613d05b2
Green <name>
2022-02-03 21:34:47 +0200

    Lesson16

--- Lesson14.py
+++ Lesson14.py
@@ -44,113 +44,117 @@ print(s.capitalize())
 # s = 'Helllo123'
 # print(s.isalnum())
 
-number = '0123456789'
-spe = '!?,.'
-s = 'some interesting, text and else! what i am doing. how do you do? i am fine! thank you.'
-count_number = 0
-count_symbol = 0
-count_spe = 0
-count_spea = 0
-for i in range(len(s)):
-    print(s[i])
-    a = s[i]
-    for n in range(len(number)):
-        if a == number[n]:
-            count_number = count_number + 1
-    for q in range(len(spe)):
-        if a == spe[q]:
-            count_spe = count_spe + 1
-    if a == spe[0]:
-        count_spea = count_spea + 1
-print(count_symbol, count_number, count_spe,count_spea)
+# number = '0123456789'
+# spe = '!?,.'
+# s = 'some interesting, text and else! what i am doing. how do you do? i am fine! thank you.'
+# count_number = 0
+# count_symbol = 0
+# count_spe = 0
+# count_spea = 0
+# for i in range(len(s)):
+#     print(s[i])
+#     a = s[i]
+#     for n in range(len(number)):
+#         if a == number[n]:
+#             count_number = count_number + 1
+#     for q in range(len(spe)):
+#         if a == spe[q]:
+#             count_spe = count_spe + 1
+#     if a == spe[0]:
+#         count_spea = count_spea + 1
+# print(count_symbol, count_number, count_spe,count_spea)
+#
+# t = 'some interesting, text and else. what i am doing. how do you do? i am fine! thank you.'
+# t = t.capitalize()
+# symbols = '!.?'
+# for i in range(2, len(t)):
+#     for s in range(len(symbols)):
+#         print(t[i - 2:i])
+#         if t[i - 2:i] == (symbols[s] + ' '):
+#             # print('y')
+#             t = t[:i] + t[i].upper() + t[i + 1:]
+# print(t)
+# s = 'Hello World!'
+# print(s[0:5])#Hello
+# print(s[-6:-1])#World
+# print(s[-6:])#World!
+# print(s[:5])#Hello
+# print(s[::-1])#!dlroW olleH
+#
+# word = 'Hello World!'
+# for letter in word:
+#     print(letter)
+#
+# #in
+# number = '0123456789'
+# spe = '!?,.'
+# s = 'some interesting, text and else! what i am doing. how do you do? i am fine! thank you.'
+# count_number = 0
+# count_symbol = 0
+# count_spe = 0
+# count_spea = 0
+# for i in range(len(s)):
+#     print(s[i])
+#     a = s[i]
+#
+#     if a in number:
+#         count_number = count_number + 1
+#     if a in spe:
+#         count_spe = count_spe + 1
+#     if a == spe[0]:
+#         count_spea = count_spea + 1
+# print(count_symbol, count_number, count_spe,count_spea)
+#
+# word = 'Hello World!'
+# print(word.find('l'))
+# print(word.rfind('l'))
+#
+# #https://docs.python.org/3/library/stdtypes.html#string-methods
+# #https://pythonworld.ru/tipy-dannyx-v-python/stroki-funkcii-i-metody-strok.html
+#
+# weight = '44'#str(input('Enter your weight : '))
+# if weight.isnumeric():
+#     weight = int(weight)
+#     if weight < 0:
+#         print('error-')
+#     elif weight > 150:
+#         print('error+')
+#     elif weight < 150:
+#         print('Your weight is', weight)
+#     else:
+#         print('Oversize')
+#
+#
+# n = 0
+# maximum = -10000000
+# minimum = -10000000
+# count = 0
+# summa = 0
+# av = 0
+# while n != 7:
+#     if maximum == -10000000:
+#         maximum = n
+#     if minimum == -10000000:
+#         minimum = n
+#     n = int(input("Enter number : "))
+#     if n > maximum:
+#         maximum = n
+#     if n < minimum:
+#         minimum = n
+#     count = count + 1
+#     summa = summa + n
+#     av = summa / count
 
-t = 'some interesting, text and else. what i am doing. how do you do? i am fine! thank you.'
-t = t.capitalize()
-symbols = '!.?'
-for i in range(2, len(t)):
-    for s in range(len(symbols)):
-        print(t[i - 2:i])
-        if t[i - 2:i] == (symbols[s] + ' '):
-            # print('y')
-            t = t[:i] + t[i].upper() + t[i + 1:]
-print(t)
-s = 'Hello World!'
-print(s[0:5])#Hello
-print(s[-6:-1])#World
-print(s[-6:])#World!
-print(s[:5])#Hello
-print(s[::-1])#!dlroW olleH
-
-word = 'Hello World!'
-for letter in word:
-    print(letter)
-
-#in
-number = '0123456789'
-spe = '!?,.'
-s = 'some interesting, text and else! what i am doing. how do you do? i am fine! thank you.'
-count_number = 0
-count_symbol = 0
-count_spe = 0
-count_spea = 0
-for i in range(len(s)):
-    print(s[i])
-    a = s[i]
-
-    if a in number:
-        count_number = count_number + 1
-    if a in spe:
-        count_spe = count_spe + 1
-    if a == spe[0]:
-        count_spea = count_spea + 1
-print(count_symbol, count_number, count_spe,count_spea)
-
-word = 'Hello World!'
-print(word.find('l'))
-print(word.rfind('l'))
-
-#https://docs.python.org/3/library/stdtypes.html#string-methods
-#https://pythonworld.ru/tipy-dannyx-v-python/stroki-funkcii-i-metody-strok.html
-
-weight = '44'#str(input('Enter your weight : '))
-if weight.isnumeric():
-    weight = int(weight)
-    if weight < 0:
-        print('error-')
-    elif weight > 150:
-        print('error+')
-    elif weight < 150:
-        print('Your weight is', weight)
-    else:
-        print('Oversize')
-
-
-n = 0
-maximum = -10000000
-minimum = -10000000
-count = 0
-summa = 0
-av = 0
-while n != 7:
-    if maximum == -10000000:
-        maximum = n
-    if minimum == -10000000:
-        minimum = n
-    n = int(input("Enter number : "))
-    if n > maximum:
-        maximum = n
-    if n < minimum:
-        minimum = n
-    count = count + 1
-    summa = summa + n
-    av = summa / count
-
-s = ''
-while True:
-    num = input('Please, enter a number:')
-    for i in range(len(num)):
-        if num[i] == '6' or num[i] == '3':
-            continue
-        else:
-            s = s + num[i]
-print(int(s))
+# s = ''
+# while True:
+#     num = input('Please, enter a number:')
+#     for i in range(len(num)):
+#         if num[i] == '6' or num[i] == '3':
+#             continue
+#         else:
+#             s = s + num[i]
+# print(int(s))
+s1 = '123445687'
+s1= s1.replace('3','')
+s1= s1.replace('6','')
+print(s1)
