959b3af6ed7cf40555a95b77df486c1fc22e23ad
Green <name>
2022-02-10 21:37:10 +0200

    Lesson20

---
+++ afterparty.py
@@ -0,0 +1,17 @@
+#Afterparty
+# l = [1,2,3,4,4,5,6,7,8,9,0,1,3]
+# for i in range(1,len(l)):
+#     if l[i] == l[i-1]:
+#         l[i] = ''
+# while '' in l:
+#     l.remove('')
+# print(l)
+# l = [1,2,3,4,4,5,6,7,8,9,0,1,3]
+# a = 1
+# length = len(l)
+# while a < length:
+#     if l[a] == l[a-1]:
+#         l.pop(a)
+#     a = a + 1
+#     length = len(l)
+# print(l)
