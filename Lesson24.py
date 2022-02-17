a6a178fa194c12aaaf056e47695b70076d317034
Green <name>
2022-02-17 21:05:09 +0200

    Lesson24

--- Lesson24.py
+++ Lesson24.py
@@ -7,6 +7,7 @@ driver_path_chrome = SC(os.getcwd() + '\drivers\chromedriver.exe')
 driver_path_firefox = SF(os.getcwd() + '\drivers\geckodriver.exe')
 
 driver = webdriver.Firefox(service=driver_path_firefox)
+#driver = webdriver.Chrome(service=driver_path_chrome)
 url = 'https://core.telegram.org/'
 driver.get(url)
 # driver.set_window_position(1600,300)#Для второго монитора расположенного справа
@@ -16,3 +17,15 @@ driver.maximize_window()
 # driver.set_window_size(300,100)
 url = 'https://test.mensa.no/'
 driver.get(url)
+print(driver.title)
+button_18_50_xpath = '/html/body/div[2]/main/cach/div[1]/div/div/div[2]/div/button[2]'
+button_51_60_xpath = '/html/body/div[2]/main/cach/div[1]/div/div/div[2]/div/button[3]'
+button_51_60 = driver.find_element('xpath',button_51_60_xpath)
+print(button_51_60.text)
+button_18_50 = driver.find_element('xpath',button_18_50_xpath)
+print(button_18_50.text)
+button_18_50.click()
+button_start_xpath = '//*[@id="startTest"]'
+button_start_xpath_full = '/html/body/div[2]/main/cach/div[2]/div/div/div/div[2]/button'
+button_start = driver.find_element('xpath',button_start_xpath_full)
+button_start.click()
