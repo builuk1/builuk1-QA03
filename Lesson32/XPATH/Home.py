d962561cecbae4e0e4b74483f5429947c3a2bba4
Green <name>
2022-04-26 21:05:48 +0300

    Lesson32

--- Lesson32/XPATH/Home.py
+++ Lesson32/XPATH/Home.py
@@ -1,5 +1,5 @@
 from_start = '//span/a[contains(@href,"/refactoring/what-is-refactoring")]'
 dirty_code = '//span/a[contains(@href,"/refactoring/technical-debt")]'
 how_to = '//span/a[contains(@href,"/refactoring/how-to")]'
-ru_language = '//a[@href="https://refactoring.guru/ru"]'
-ua_language = '//a[@href="https://refactoring.guru/ua"]'
+ru_language = '//div[@class="nano-content"]/div/a[contains(@href,"https://refactoring.guru/ru")]'
+ua_language = '//div[@class="nano-content"]/div/a[contains(@href,"https://refactoring.guru/uk")]'
